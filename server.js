8a17498d5a453120c1c6bda24261f9b3d48d73b9
Benjamin Arruebarrena <arruebarrena15@gmail.com>
2021-09-11 16:49:25 -0300

    acrecentando o mongoose

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express()
 const porta = 300
+const mongoose = require("mongoose")
 
 app.get('/', (req,res) => {
     res.send('hola locos')
 }) 
-app.listen(porta);
+app.listen(porta, () => {
+    console.log(`roando seridor na porta ${porta}`);
+});
